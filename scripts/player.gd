0a9c59e9b7197adec98607b0d8112c15344cbeb4
Alex Petrean <alex.petrean@outlook.com>
2023-06-03 03:58:02 +0300

    tutorial done

---
+++ scripts/player.gd
@@ -0,0 +1,65 @@
+extends Area2D
+signal hit
+
+@export var speed = 400
+var screen_size
+
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+	hide()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()
+
+	velocity = process_inputs(velocity)
+
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
+
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	change_animation(velocity)
+
+
+func _on_body_entered(_body):
+	hide()
+	hit.emit()
+	$CollisionShape2D.set_deferred("disabled", true)
+
+
+func process_inputs(velocity):
+	if Input.is_action_pressed("moveRight"):
+		velocity.x += 1
+	if Input.is_action_pressed("moveLeft"):
+		velocity.x -= 1
+	if Input.is_action_pressed("moveDown"):
+		velocity.y += 1
+	if Input.is_action_pressed("moveUp"):
+		velocity.y -= 1
+
+	return velocity
+
+
+func change_animation(velocity):
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "up"
+		$AnimatedSprite2D.flip_v = velocity.y > 0
